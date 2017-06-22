d2ff2a51f098379f79c0d2cf544d914cebc9a1cf
Alicia <Alicia@LAPTOP-RU5TT05C.hfu-open.fu.hs-furtwangen.de>
2017-06-22 15:34:40 +0200

    update

--- NodeTest.ts
+++ NodeTest.ts
@@ -42,7 +42,7 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     for (key in query) {
         console.log(key + ":" + query[key]);
         if (key == "Vanille" || key == "Erdbeere" || key == "Schokolade" || key == "Walnuss") {
-            _response.write(key +" = " + query[key] + "<br>");
+            _response.write(key + " Test" + query[key] + "<br>");
         }
         
     
