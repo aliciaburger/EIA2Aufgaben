1811dcd4eb06f4a75313f521472c3c701d8dbb87
Alicia <Alicia@LAPTOP-RU5TT05C>
2017-06-25 15:44:48 +0200

    update

--- Node10.ts
+++ Node10.ts
@@ -16,6 +16,7 @@ let server: Http.Server = Http.createServer();
 server.addListener("listening", handleListen);
 server.addListener("request", handleRequest);
 //server.listen(process.env.port||8100);
+
 server.listen(port);
 
 function handleListen(): void {
@@ -42,7 +43,10 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
     for (key in query) {
         console.log(key + ":" + query[key]);
         if (key == "Vanille" || key == "Erdbeere" || key == "Schokolade" || key == "Walnuss") {
-            _response.write(key + "\n" + query[key] + "<br>");
+            if(parseInt(query[key]) != 0) {
+                _response.write(key + "\n" + query[key] + "<br>");
+            }
+            
             //Test
         }
         
