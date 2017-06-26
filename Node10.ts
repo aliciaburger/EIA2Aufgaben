11355b1ccbbde597f001a6d72331459fc5b65eb6
Alicia <Alicia@LAPTOP-RU5TT05C.hfu-open.fu.hs-furtwangen.de>
2017-06-26 11:13:46 +0200

    Test build

--- Node10.ts
+++ Node10.ts
@@ -50,33 +50,12 @@ function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerRes
                 _response.write(key + "\n" + query[key] + "<br>");
 
             }
-
-            //Test
-
+//TEST xxxxxxx
         }
-        
 
-      
-
-        //    _response.write("ich höre Stimmen!");
-        //    _response.write("ich höre Stimmen!");
-        
     }
-_response.write("Ihre angegebene Lieferadresse: " + "<br>" + query["Strasse"] + "<br>" + query["Hausnummer"] + "<br>" + query["Stadt"] + "<br>" + query["Postleizahl"] + "<br>");
-_response.write("Die Rechnung mit Bestellübersicht, wurde ihnen bereits an " + query["EMail"] + "gesendet.");
-_response.end();
+    _response.write("Ihre angegebene Lieferadresse: " + "<br>" + query["Strasse"] + "<br>" + query["Hausnummer"] + "<br>" + query["Stadt"] + "<br>" + query["Postleizahl"] + "<br>");
+    _response.write("Die Rechnung mit Bestellübersicht, wurde ihnen bereits an " + query["EMail"] + "gesendet.");
+    _response.end();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
