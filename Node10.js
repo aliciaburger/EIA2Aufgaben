eefb46a912752ff403c9988b3cb936f9e4df6d17
Alicia <Alicia@LAPTOP-RU5TT05C.hfu-open.fu.hs-furtwangen.de>
2017-06-26 11:26:41 +0200

    upload

--- Node10.js
+++ Node10.js
@@ -1,61 +1,41 @@
+"use strict";
 console.log("Server starting");
 //TEST
-import Http = require("http");
-import Url = require("url");
-
-interface AssocStringString {
-    [key: string]: string;
-}
-
-let port: number = process.env.PORT;
+const Http = require("http");
+const Url = require("url");
+let port = process.env.PORT;
 if (port == undefined)
     port = 8100;
-
-let server: Http.Server = Http.createServer();
-
+let server = Http.createServer();
 server.addListener("listening", handleListen);
 server.addListener("request", handleRequest);
 //server.listen(process.env.port||8100);
-
 server.listen(port);
-
-function handleListen(): void {
+function handleListen() {
     console.log("Listening on port: " + port);
 }
-
-function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+function handleRequest(_request, _response) {
     console.log("Request received");
     //    _response.write("huhu");
     console.log(_request.url);
-
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-
-
-    let query: AssocStringString = Url.parse(_request.url, true).query;
+    let query = Url.parse(_request.url, true).query;
     console.log(query);
-
-
-    let key: string;
-
-
+    let key;
     _response.write("Hallo Frau/ Herr " + query["Nachname"]);
-
     for (key in query) {
-
         console.log(key + ":" + query[key]);
         if (key == "Vanille" || key == "Erdbeere" || key == "Schokolade" || key == "Walnuss") {
             if (parseInt(query[key]) != 0) {
                 _response.write("Sie haben bei uns Folgende Eissorten bestellt:" + "<br>");
                 _response.write(key + "\n" + query[key] + "<br>");
-
             }
         }
-
     }
     // Kommentar! XXX
     _response.write("Ihre angegebene Lieferadresse: " + "<br>" + query["Strasse"] + "<br>" + query["Hausnummer"] + "<br>" + query["Stadt"] + "<br>" + query["Postleizahl"] + "<br>");
     _response.write("Die Rechnung mit Bestellübersicht, wurde ihnen bereits an " + query["EMail"] + "gesendet.");
     _response.end();
 }
-
+//# sourceMappingURL=Node10.js.map
